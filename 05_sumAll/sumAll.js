81a308d6ec6ea004656b41a4a7567147003221a6
Ekio <141391736+Eki213@users.noreply.github.com>
2025-04-12 19:03:01 -0300

    fix: make sumAll() pass all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if ((start < 0 || end < 0 ) || 
+        (!Number.isInteger(start) || !Number.isInteger(end))
+    ) return 'ERROR'; 
+    if (start > end) [start, end] = [end, start];
 
+    let sum = 0;
+    for (;start <= end; start++) sum += start;
+    return sum;
 };
 
 // Do not edit below this line
