fb46d77de6600d498cfea063dcea2ff0eeec39a6
Ekio <141391736+Eki213@users.noreply.github.com>
2025-04-22 18:05:26 -0300

    Make findTheOldest.js functions pass all tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((oldestPerson, currentPerson) => {
+        const currentPersonAge = (currentPerson.yearOfDeath || new Date().getFullYear()) - currentPerson.yearOfBirth;
+        const oldestPersonAge = (oldestPerson.yearOfDeath || new Date().getFullYear()) - oldestPerson.yearOfBirth;
 
+        return (currentPersonAge > oldestPersonAge) ? currentPerson: oldestPerson;
+    });
 };
 
 // Do not edit below this line
