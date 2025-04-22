ad3b5bcc8c85881651b4542af7e9be4989f5813d
Ekio <141391736+Eki213@users.noreply.github.com>
2025-04-22 16:28:06 -0300

    fix: make fibonacci.js functions pass all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (+num == 0) return 0;
+    if (+num < 0) return 'OOPS';
 
+    let firstPrev = 1;
+    let lastPrev = 1;
+    let current = 1;
+
+    for (let i = 2; i < num; i++) {
+        current = firstPrev + lastPrev;
+        lastPrev = firstPrev;
+        firstPrev = current;
+    }
+
+    return current;
 };
 
 // Do not edit below this line
