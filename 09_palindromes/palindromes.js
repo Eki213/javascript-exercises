e2e46e9e1e0e3412f1cd3028c1b81fd87a761ec8
Ekio <141391736+Eki213@users.noreply.github.com>
2025-04-22 14:04:09 -0300

    fix: make palindromes.js functions pass all tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const validChars = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+    const cleanedString = str
+        .toLowerCase()
+        .split('')
+        .filter(char => validChars.includes(char))
+        .join('');
+    
+    return cleanedString.split('').reverse().join('') === cleanedString;
 };
 
 // Do not edit below this line
