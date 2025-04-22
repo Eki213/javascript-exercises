db70e1756e00b8afae98b24e50f267fe256becd6
Ekio <141391736+Eki213@users.noreply.github.com>
2025-04-22 09:42:29 -0300

    fix: make calculator.js functions pass all tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,27 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, num) => sum + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((res, num) => res * num, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let product = 1;
+  for (let i = num; i >= 2; i--) product *= i;
+  return product;
 };
 
 // Do not edit below this line
